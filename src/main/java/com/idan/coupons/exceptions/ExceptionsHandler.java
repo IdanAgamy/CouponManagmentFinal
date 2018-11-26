e8e8f3193f880aca87008022c574f82d55e0d4ad
IdanAgamy <42001487+IdanAgamy@users.noreply.github.com>
2018-11-26 23:34:39 +0900

    Update ExceptionsHandler.java

--- src/main/java/com/idan/coupons/exceptions/ExceptionsHandler.java
+++ src/main/java/com/idan/coupons/exceptions/ExceptionsHandler.java
@@ -4,32 +4,40 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.idan.coupons.beans.ApplicationError;
 import com.idan.coupons.enums.ErrorType;
 
+@ResponseBody
 @ControllerAdvice
 public class ExceptionsHandler{
-
+	
 	@ExceptionHandler(ApplicationException.class)
-	public void handleApplicationException(HttpServletResponse response, ApplicationException exception) {
-		String errorMessage = exception.toString();
+	public ApplicationError handleApplicationException(HttpServletResponse response, ApplicationException exception) {
+//		String errorMessage = exception.toString();
+		ApplicationError error = new ApplicationError(exception.getType().getNumber(),exception.getType().name(), exception.getMessage());
+		if (exception.getType() == ErrorType.INVALID_PARAMETER) {
+			error.setInputErrorTypes(exception.getTypes()); 
+		}
 		if(exception.getType() == ErrorType.SYSTEM_ERROR) {
 			//TODO implement logger.
 			exception.printStackTrace();
 		}
 		response.setStatus(exception.getType().getNumber());
-		response.setHeader("errorMessage", errorMessage);
-
+		return error;
 
 	}
 
 	@ExceptionHandler(Exception.class)
-	public void handleGeberalException(HttpServletResponse response, Exception exception) {
-		String errorMessage = exception.toString();
+	public ApplicationError handleGeberalException(HttpServletResponse response, Exception exception) {
+//		String errorMessage = exception.toString();
+		int errorCode = 500;
+		ApplicationError error = new ApplicationError(errorCode, ErrorType.SYSTEM_ERROR.name(), exception.getMessage());
 		exception.printStackTrace();
 		//TODO implement logger.
-		response.setStatus(500);
-        response.setHeader("errorMessage", errorMessage);
+		response.setStatus(errorCode);
+        return error;
 	}
 
 }
